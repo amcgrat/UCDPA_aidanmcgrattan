e0930d165565b5565b59a0d899728ee31334e2ef
amcgrat <aidan_mcgrattan@uhc.com>
2021-03-06 17:17:33 +0000

    Connection and filtering for project 3/6/21.

--- UCD_PROJECT.py
+++ UCD_PROJECT.py
@@ -2,9 +2,9 @@ from sodapy import Socrata
 import pandas as pd
 
 client = Socrata("data.cms.gov", None)
-results = client.get('efgi-jnkv',select ='hcpcs_cd,psps_denied_services_cnt', where = 'psps_denied_services_cnt >0',limit = 15000000)
+results = client.get('efgi-jnkv',select ='hcpcs_cd,psps_denied_services_cnt', where = "psps_denied_services_cnt >30 AND hcpcs_cd='99213'",limit = 15000000)
 client.close()
 results_df = pd.DataFrame.from_records(results)
 print(results_df.info())
 print(results_df.describe())
-
+print(results_df.head(20))
